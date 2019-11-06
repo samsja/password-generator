b9d3fc123886962af066e62be06905747696afcf
Sami Jaghouar <sami.jaghouar@polynom.io>
2019-11-06 15:03:45 +0100

    add help command

--- password-gen.py
+++ password-gen.py
@@ -4,6 +4,7 @@ import sys
 
 
 default_value = 20
+help = False
 
 def randomStringDigits(stringLength=default_value):
     """Generate a random string of letters and digits """
@@ -11,9 +12,18 @@ def randomStringDigits(stringLength=default_value):
     return ''.join(random.choice(lettersAndDigits) for i in range(stringLength))
 
 try:
-    n=int(sys.argv[-1])
+    arg = sys.argv[-1]
+    if arg in ["--help","-h"]:
+        help =True
+    else:
+        n=int(arg)
 
 except ValueError:
     n= default_value
 
-print(randomStringDigits(n))
+if help:
+    print("------password-generator----------")
+    print("* password-gen n generate a random password with a length n")
+    print("* the default value is n=20")
+else:
+    print(randomStringDigits(n))
